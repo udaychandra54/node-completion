5f0c0df14aeb6e89c48bc7c3ecd2d20573234860
Udaya Jillellamudi <udajille@WKMZTFDACA3E.global.publicisgroupe.net>
2022-10-12 09:36:21 +0530

    pushing node js with express code

---
+++ util/database.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql2");
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: "Uday@1995",
+});
+
+module.exports = pool.promise();
